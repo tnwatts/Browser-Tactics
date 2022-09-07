7758448e09ebfb5cbca1531fbe1d449ff1515e1d
tnwatts <tnwatts@live.com>
2022-09-07 15:42:24 -0600

    styling nav and profile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
 
 export default function NavBar({ user, setUser }) {
@@ -9,14 +9,23 @@ export default function NavBar({ user, setUser }) {
   }
 
   return (
-    <nav>
-      <Link to='/profile'>Profile</Link>
+  <nav className="nav-bar dark-background p-2 mb-2">
+    <ul className="nav nav-tabs">
+  <NavLink className={a => "nav-link" + (a ? " bg-secondary" : "") } to='/Profile'>Profile</NavLink>
+  <li className="nav-item">
+    <NavLink className="nav-link light-background" to='/Game'>Game</NavLink>
+  </li>
+  <li className="nav-item">
+   <NavLink className="nav-link" to='' onClick={handleLogOut}>Log Out</NavLink>
+  </li>
+</ul>
+      {/* 
       &nbsp; | &nbsp;
       <Link to='/Game'>Game</Link>
       &nbsp; | &nbsp;
       Welcome, {user.name}
       &nbsp; | &nbsp;
-      <Link to='' onClick={handleLogOut}>Log Out</Link>
+      */}
     </nav>
   );
 }
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 import { createGame } from '../../utilities/games-api';
 import { useState, useEffect } from "react"
 import { getProfile } from '../../utilities/users-api';
-import { Link } from 'react-router-dom';
+import { Link, Navigate, useNavigate } from 'react-router-dom';
 
 export default function Profile({user}) {
     const [profile,setProfile] = useState({})
@@ -19,6 +19,7 @@ export default function Profile({user}) {
     async function handleNewGame (evt) {
         evt.preventDefault();
         await createGame(user._id);
+        this("../success", { replace: true }).useNavigate()
     }
     
 
@@ -26,7 +27,7 @@ export default function Profile({user}) {
     return(
         <div>
 
-        { (!profile.gameStatus  ) &&  <button className="btn btn-primary" onClick={handleNewGame}>NEW GAME</button>}
+        { (!profile.gameStatus  ) &&  <button className="btn btn-primary btn-outline-warning" onClick={handleNewGame}>NEW GAME</button>}
         <div>Hello {profile.name}</div>
         <div> Game Status: {profile.gameStatus ?  (`You are player ${profile.gameStatus===1 ? 'one' : 'two'}`) : "You are not in a game"}</div>
         <Link to={'/game'}>To Game!</Link>
