3760b3b3c998acd8e0f58ad89f983bde39fdef89
tnwatts <tnwatts@live.com>
2022-09-14 12:10:41 -0600

    fixing readme

--- src/components/GameBoard/Token.jsx
+++ src/components/GameBoard/Token.jsx
@@ -1,16 +1,13 @@
 import "./Token.css"
 import TokenMenu from "./TokenMenu"
-import { useRef } from 'react'
 
 
-export default function Token({unit, player, pos,handleClick, damage, u, game, turn}) {
-    let tokens = useRef({})
-    console.log( turn, player,game)
+export default function Token({unit, player, pos,handleClick, damage, game, turn}) {
 
     return (
         <div className={` player${(player+1)} ${(turn === true) ? "turn" : ""} token`} 
         style={{left: pos[0], top: pos[1]}}>
-        <TokenMenu u={u} />
+        <TokenMenu u={unit} />
             <img className='image' name={unit}  src={`${unit.image}`} onClick={()=>{handleClick(unit)}}  alt=''></img>
             <div className="health-bar light-lifted pb-1 mb-1">
                 <div className="bar" style={{width:`${damage*100}%`}} ></div>
--- src/components/Profile/NameChanger.jsx
+++ src/components/Profile/NameChanger.jsx
@@ -1,31 +1,51 @@
-import { useState } from 'react';
-import { setName, deleteUser } from '../../utilities/users-api';
-import { logOut } from '../../utilities/users-service';
+import { useState } from "react";
+import { setName, deleteUser } from "../../utilities/users-api";
+import { logOut } from "../../utilities/users-service";
 
-export default function NameChanger({user, setProfile, setUser, navigate}) {
-    const [newName, setNewName] = useState('')
+export default function NameChanger({ user, setProfile, setUser, navigate }) {
+  const [newName, setNewName] = useState("");
 
-    async function handleSubmit(evt) {
-        evt.preventDefault();
-          const newP = await setName(user._id,newName);
-          setProfile(newP)
-        } 
-    console.log(newName)
-    return(
-        <div>
-        <div className="">
-          <form className="form-control dark-background light-text  lifted my-3 mx-auto border border-primary border-1" autoComplete="off" onSubmit={handleSubmit}>
-            <label className="input-label">New Name</label>
-            <input className="input-group rounded-2 bg-primary text-light" type="text" name="name"  onChange={(evt)=> setNewName(evt.target.value)} required />
-            <button className="lighten-area col-3 btn darker-background my-2  btn-outline-info light-lifted fw-bold" type="submit">Set</button>
-          </form>
-        </div>
-        <button onClick={()=> {
+  async function handleSubmit(evt) {
+    evt.preventDefault();
+    const newP = await setName(user._id, newName);
+    setProfile(newP);
+  }
+  console.log(newName);
+  return (
+    <div>
+      <div className="">
+        <form
+          className="form-control dark-background light-text  lifted my-3 mx-auto border border-primary border-1"
+          autoComplete="off"
+          onSubmit={handleSubmit}
+        >
+          <label className="input-label">New Name</label>
+          <input
+            className="input-group rounded-2 bg-primary text-light"
+            type="text"
+            name="name"
+            onChange={(evt) => setNewName(evt.target.value)}
+            required
+          />
+          <button
+            className="lighten-area col-3 btn darker-background my-2  btn-outline-info light-lifted fw-bold"
+            type="submit"
+          >
+            Set
+          </button>
+        </form>
+      </div>
+      <button
+        onClick={() => {
           deleteUser(user._id);
           logOut();
           setUser(null);
-          navigate('/Game', { replace: true })
-          }} className="vw-90 light-text text-danger dark-backgroundg ">Delete</button> 
-      </div>
-    )
+          navigate("/Game", { replace: true });
+        }}
+        className="vw-90 light-text text-danger dark-backgroundg "
+      >
+        Delete
+      </button>
+    </div>
+  );
 }
